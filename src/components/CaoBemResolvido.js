5b9760cd2e237663ad91071c895c18c9b5a520ad
Wesley <wesley.brazil@outlook.com>
2022-06-10 10:04:20 -0300

    Main: adicionada a sessão do Método

---
+++ src/components/CaoBemResolvido.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+const corPrimaria = "#f40", corSecundaria = "#535796"; 
+
+
+export const Depoimentos = styled.div`
+  width: 75%;
+  margin: 0px auto;
+  padding: 40px 0;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+
+  h2 {
+    line-height: 1.3em;
+    text-align: center;
+    color: ${corSecundaria};
+    font-size: 24px;
+    font-weight: 900;
+    text-transform: uppercase;
+    max-width: 40ch;
+    margin: auto;
+  }
+
+  img {
+    margin: 25px auto;
+    border-radius: 8px;
+    box-shadow: -1px 2px 8px #aaa;
+  }
+
+  sub {
+    margin: 10px auto;
+    line-height: 1em;
+    text-align: center;
+    font-size: 0.8em;
+    max-width: 50ch;
+  }
+`;
--- src/views/CaoBemResolvido.js
+++ src/views/CaoBemResolvido.js
@@ -1,4 +1,11 @@
 import "./styles/CaoBemResolvido.css";
+import {FaWhatsapp} from "react-icons/fa";
+
+import {Depoimentos} from "../components/CaoBemResolvido.js";
+
+import depo1 from "./styles/assets/img/depoimento1.png";
+import depo2 from "./styles/assets/img/depoimento2.png";
+import depo3 from "./styles/assets/img/depoimento3.png";
 
 export default function CaoBemResolvido(){
   return(
@@ -17,8 +24,41 @@ export default function CaoBemResolvido(){
         </div>
       </header>
       <main>
-        <div></div>
-
+        <div className="containerMain">
+          <section className="resultadosUm">
+            <div className="bgResultadosUm">
+              <Depoimentos>
+                <h2>Donos de cães de todo Brasil já tiveram suas vidas e de seus amiguinhos transformadas</h2>
+                <img src={depo1} alt="Depoimento 1" width="60%" height="70%"/>
+                <img src={depo2} alt="Depoimento 2" width="60%" height="70%"/>
+                <img src={depo3} alt="Depoimento 3" width="60%" height="70%"/>
+                <sub>Esses(as) são alunos(as) do Cão Bem Resolvido que se dedicaram e seguiram as instruções do programa. Os resultados variam para cada pessoa.</sub>
+              </Depoimentos>
+            </div>
+          </section>
+          <section className="metodo">
+            <div className="conteudoMetodo">
+              <h2>O único curso que você precisa para se dar bem com seu melhor amigo!</h2>
+              <p>Eu desenvolvi o treinamento Cão Bem Resolvido <strong>para você conseguir finalmente conseguir ter um cão adestrado de uma vez por todas</strong>.</p>
+              <p>São várias vídeo aulas de 5-10 minutos em que eu te mostro na prática como eu adestro cães e <strong>como você resolverá problemas como xixi e cocô, mordidas, chorar quando fica sozinho, passeio puxando e muito mais</strong>.</p>
+              <p>Você vai precisar de <strong>apenas de 15 minutinhos por dia</strong> para aplicar as minhas técnicas e resolver os seus problemas com o seu melhor amigo.</p>
+              <p>Vou te mostrar como você vai conseguir resolver todos os problemas que você está passando com o seu cão de <strong>forma muito fácil e didática</strong>.</p>
+              <p>Além disso eu vou te dar um <strong>suporte personalizado no meu Grupo VIP</strong>, lá eu tiro todas as dúvidas dos alunos diariamente.</p> 
+              <p>Agora é com você, estou te esperando!</p>
+            </div>
+          </section>
+          <section className="depoimentoUm"></section>
+          <section className="modulos"></section>
+          <section className="bonus"></section>
+          <section className="resultadosDois"></section>
+          <section className="garantia"></section>
+          <section className="futurePassing"></section>
+          <section className="resultadosTres"></section>
+          <section className="midia"></section>
+          <section className="cta"></section>
+          <section className="faq"></section>
+        </div>
+        <a id="whatsapp" href="https://wa.me/message/ESYSX4KPAHWXH1" rel="noreferrer" target="_blank"><FaWhatsapp size={"70%"}/></a>
       </main>
     </>
   )
