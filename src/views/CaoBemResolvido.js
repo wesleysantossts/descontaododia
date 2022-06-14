eaaf89a74724e56165eb75bb5d95e926371f5cd0
Wesley <wesley.brazil@outlook.com>
2022-06-14 01:43:37 -0300

    Develop: atualização develop

--- src/views/CaoBemResolvido.js
+++ src/views/CaoBemResolvido.js
@@ -28,13 +28,13 @@ export default function CaoBemResolvido(){
       <header className="headerCBR">
         <div className="bgHeader">
           <div className="containerHeader">
-            <h1>DESCUBRA COMO ADESTRAR O SEU PRÓPRIO CÃO EM ATÉ 14 DIAS!</h1><br/><br/><br/>
+            <h1>DESCUBRA COMO ADESTRAR O SEU PRÓPRIO CÃO EM ATÉ 14 DIAS</h1><br/><br/><br/>
             <p><strong>Sem brigar com ele</strong> e mesmo que você não entenda NADA de Cachorro!</p>
 
             <iframe src="https://www.youtube.com/embed/eHWPAgBzW4Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
             <button onClick={()=> window.location.assign("https://go.hotmart.com/P71561841A?ap=b082")}>Quero que meu cachorro me obedeça!</button><br/><br/>
-            <sub>Acesso por apenas 12x <strong>R$29,64</strong></sub>
+            <p>Acesso por apenas 12x <strong>R$29,64</strong></p>
           </div>
         </div>
       </header>
@@ -68,6 +68,7 @@ export default function CaoBemResolvido(){
               <p>Agora é com você, estou te esperando!</p>
               <div className="btnSatisfacao">
                 <button onClick={()=> window.location.assign("https://go.hotmart.com/P71561841A?ap=b082")}>Só 15 minutos? Eu quero!</button>
+                <p>Acesso imediato por apenas 12x <strong>R$19,66</strong></p>
                 <img src={satisfacaoGarantida} alt="satisfação garantida" width="80%"/>
               </div>
             </div>
